0e274e0449bf1fbd70eb00ad03fe7e9f235c6f6d
Cierra-McDonald <cierralauren15@gmail.com>
2021-05-18 11:16:24 -0700

    two api calls

--- src/components/services/AvatarApi.js
+++ src/components/services/AvatarApi.js
@@ -1,10 +1,18 @@
 export const getAvatars = async () => { 
 
     const res = await fetch('https://last-airbender-api.herokuapp.com/api/v1/characters?affiliation=Water+Tribe');
-    const avatars = res.json() 
+    const avatars = await res.json() 
 
     return avatars.map((avatar) => {
 
     })
 
+}
+
+export const getOneAvatar = async () => { 
+    //change character id
+    const res = await fetch('https://last-airbender-api.herokuapp.com/api/v1/characters/5cf5679a915ecad153ab6912?affiliation=Water+Tribe');
+    const oneCharacter = await res.json();
+
+    return oneCharacter;
 }
